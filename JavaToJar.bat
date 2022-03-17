be12d076573f88f72191fd058e942e7443b88d7e
lingbaojun233 <100983007+lingbaojun233@users.noreply.github.com>
2022-03-17 16:57:25 +0800

    Add files via upload

---
+++ JavaToJar.bat
@@ -0,0 +1,16 @@
+javac com\kdxxcx\util\Array\*.java -encoding UTF-8
+javac com\kdxxcx\util\AWTAndSwing\*.java -encoding UTF-8
+javac com\kdxxcx\util\Class\*.java -encoding UTF-8
+javac com\kdxxcx\util\String\*.java -encoding UTF-8
+javac com\kdxxcx\util\Exception\*.java -encoding UTF-8
+javac com\kdxxcx\util\Integer\*.java -encoding UTF-8
+javac com\kdxxcx\util\File\*.java -encoding UTF-8
+javac com\kdxxcx\util\KDXXCX\*.java -encoding UTF-8
+javac com\kdxxcx\util\SQL\*.java -encoding UTF-8
+javac com\kdxxcx\util\String\*.java -encoding UTF-8
+javac com\kdxxcx\util\Time\*.java -encoding UTF-8
+javac com\kdxxcx\programme\*.java -encoding UTF-8
+javac com\kdxxcx\programme\Class\*.java -encoding UTF-8
+javac com\kdxxcx\programme\GUI\*.java -encoding UTF-8
+jar -cvfm 快递信息查询2.0.1.jar META-INF\MANIFEST.MF com\kdxxcx\util\Array\*.class com\kdxxcx\util\AWTAndSwing\*.class com\kdxxcx\util\Class\*.class com\kdxxcx\util\Exception\*.class com\kdxxcx\util\File\*.class com\kdxxcx\util\Integer\*.class com\kdxxcx\util\KDXXCX\*.class com\kdxxcx\util\SQL\*.class com\kdxxcx\util\String\*.class com\kdxxcx\util\Time\*.class com\kdxxcx\programme\*.class com\kdxxcx\programme\Class\*.class com\kdxxcx\programme\GUI\*.class com\microsoft kdxxcx.update mssql microsoft
+jar -cvfm CTCODES.lingbaojun META-INF\MANIFEST.MF com kdxxcx.update mssql microsoft
